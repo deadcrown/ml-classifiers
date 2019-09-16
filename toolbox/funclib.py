04cf0fda90e93700a3a2027ff4a9c915f2a1fda7
Ahmad Faiz <faiz.ju93@gmail.com>
2019-09-16 20:04:31 +0530

    Function to plot decision boundary

---
+++ toolbox/funclib.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, Y, clsfr, res=0.2):
+    
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColorMap(colors[:len(np.unique(y))])
+
+    #plot decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 0].max() + 1
+    xx1, xx2 = np.meshgrid(np.arrange(x1_min, x1_max, resolution),
+            np.arrange(x2_min, x2_max, resolution))
+
+    Z = clsfr.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+
+    #plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                y=X[y == cl, 1],
+                alpha=0.8,
+                c=colors[idx],
+                markers=markers[idx],
+                label=cl,
+                edgecolor='black')
+

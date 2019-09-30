d7103e3028b27ac25d006ac67583f9486ac34909
Ahmad Faiz <faiz.ju93@gmail.com>
2019-10-01 04:07:53 +0530

    Fix weight update for each iteration by moving add_dim outside epoch loop

--- logistic/logisticclsfr.py
+++ logistic/logisticclsfr.py
@@ -4,7 +4,7 @@ use sigmoid activation function
 update loss using log loss
 '''
 
-import numpy
+import numpy as np
 
 class LogisticGD(object):
     def __init__(self, iter, eta, random_state):
@@ -23,18 +23,18 @@ class LogisticGD(object):
         return 1. / (1. + np.exp(-np.clip(z, -300, 300))) #clip limits the value of X.T.w to -300 to 300 to avoid overflow
     
     def fit(self, X, y):
-        seed = np.Random.RandomState(self.random_state)
+        seed = np.random.RandomState(self.random_state)
         # init weight drawn from random gaussian with mean 0 and variance 0.01
-        self.w_ = seed.normal(loc=0.0, variance=0.01, size=X.shape[1])
+        X = self.add_dim(X)        
+        self.w_ = seed.normal(loc=0.0, scale=0.01, size=X.shape[1])
         for epoch in range(self.n_iter):
-            X = self.add_dim(X)
             z = self.net_input(X)
             z_out = self.activation_function(z)
             err = z_out - y 
             # update weight
             self.w_ -= np.dot(X.T, self.w_)
             # -(yi(log(h(xi))) + (1-yi)(log(1-h(xi)))
-            log_loss = -np.dot(y.T, np.log(z_out)) - np.dot(1-y.T, np.log(1-z_out))
+            log_loss = -np.dot(y.T, np.log(z_out)) - np.dot(1-y.T, np.log(1-z_out)) 
             self.loss_.append(log_loss)
             print('epoch:{}\terr:{}'.format(epoch, log_loss))
         return self

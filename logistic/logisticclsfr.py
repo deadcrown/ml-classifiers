8ef58c48b0718165e1a6915c793bfe8adce3abbd
Ahmad Faiz <faiz.ju93@gmail.com>
2019-10-01 03:49:25 +0530

    Write functions for logistic class
    implement add_dim(), net_input(), activation_function(), fit(), predict()
    Use sigmoidal activation function
    Use log loss to calculate loss per batch

--- logistic/logisticclsfr.py
+++ logistic/logisticclsfr.py
@@ -13,16 +13,31 @@ class LogisticGD(object):
         self.random_state = random_state
 
     def add_dim(self, X):
-        pass
+        p = np.ones((X.shape[0],1))
+        return np.append(X, p, axis=1)
 
-    def net_input(self):
-        pass
+    def net_input(self, X):
+        return np.dot(X, self.w_)
 
-    def activation_function(self):
-        pass
+    def activation_function(self, z):
+        return 1. / (1. + np.exp(-np.clip(z, -300, 300))) #clip limits the value of X.T.w to -300 to 300 to avoid overflow
     
-    def fit(self):
-        pass
+    def fit(self, X, y):
+        seed = np.Random.RandomState(self.random_state)
+        # init weight drawn from random gaussian with mean 0 and variance 0.01
+        self.w_ = seed.normal(loc=0.0, variance=0.01, size=X.shape[1])
+        for epoch in range(self.n_iter):
+            X = self.add_dim(X)
+            z = self.net_input(X)
+            z_out = self.activation_function(z)
+            err = z_out - y 
+            # update weight
+            self.w_ -= np.dot(X.T, self.w_)
+            # -(yi(log(h(xi))) + (1-yi)(log(1-h(xi)))
+            log_loss = -np.dot(y.T, np.log(z_out)) - np.dot(1-y.T, np.log(1-z_out)))
+            self.loss_.append(log_loss)
+            print('epoch:{}\terr:{}'.format(epoch, log_loss))
+        return self
 
-    def predict(self):
-        pass
+    def predict(self, X):
+        return np.where(self.activation_function(self.net_input(self.add_dim(X))) >= 0.0, 1, -1)

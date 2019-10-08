bb96234579eae25d1d623995c4c0316b8d73a635
Ahmad Faiz <faiz.ju93@gmail.com>
2019-10-08 06:36:58 +0530

    Train majority vote on iris dataset

--- clsfr/ensemble/majorityvote_train.py
+++ clsfr/ensemble/majorityvote_train.py
@@ -1,5 +1,7 @@
 '''using iris dataset for training the ensemble
-using 2 features for viz purpose
+using 2 features for viz purpose; sepal-width, petal-length
+using 2 labels to directly calculate roc_auc without using OnevsAll approach for calculating roc_auc for multicalss classification
+target_class_labels: iris-versicolor(0), iris-virginica(1) 
 comparison is made over ROC AUC as metric for each base classifier and ensemble
 using three H; decision_tree, knn, logistic_regression
 '''
@@ -12,8 +14,8 @@ from sklearn.preprocessing import LabelEncoder
 
 # prep training dataset
 iris = datasets.load_iris()
-X = iris.data[:, [1,2]]
-y = iris.target
+X = iris.data[50:, [1,2]]
+y = iris.target[50:]
 lblenc = LabelEncoder()
 y = lblenc.fit_transform(y)
 
@@ -22,7 +24,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline
 
 knn_clf = KNeighborsClassifier(
     n_neighbors = 1,
@@ -42,18 +44,20 @@ lr_clf = LogisticRegression(
 
 # pipeline for knn and lr
 # dt doesnt required sclaed features -- scale invariant
-knn_pipe = make_pipeline(
+# Pipeline is used instaed of make_pipeline to give names to each transformation which will be used for tuning individual base in ensemble
+knn_pipe = Pipeline([
     ['ssc', StandardScaler()],
     ['clf', knn_clf]
-)
-lr_pipe = make_pipeline(
+])
+lr_pipe = Pipeline([
     ['ssc', StandardScaler()],
     ['clf', lr_clf]
-)
+])
 
 clsfr_name = ['LR', 'KNN', 'DT']
+all_clsfrs = [lr_pipe, knn_pipe, dt_clf]
 # get cv roc_auc over 10 folds for each base classifier
-for _clf_nm, _clf in zip(clsfr_name, [lr_pipe, knn_pipe, dt_clf]):
+for _clf_nm, _clf in zip(clsfr_name, all_clsfrs):
     score = cross_val_score(
         estimator = _clf,
         X = X,
@@ -63,16 +67,20 @@ for _clf_nm, _clf in zip(clsfr_name, [lr_pipe, knn_pipe, dt_clf]):
     )
     print('roc_auc over 10 folds for {}: {} +/- {}'.format(_clf_nm, np.mean(score), np.std(score)))
 
-# get ensembel classifier using majority vote principle
+# get ensemble classifier using majority vote principle
+# import tmp_majclsfr
+import majorityvote_clsfr
 from majorityvote_clsfr import MajorityVoteEnsemble
+# from tmp_majclsfr import MajorityVoteClassifier
 
-ens = MajorityVoteEnsemble(clsfrs=[lr_pipe, knn_pipe, dt_clf])
-ens_name = 'Majority_Vote'
-clsfr_name += ens_name
+ens = MajorityVoteEnsemble(classifiers=[lr_pipe, knn_pipe, dt_clf])
+# ens = MajorityVoteClassifier(classifiers=[lr_pipe, knn_pipe, dt_clf])
+clsfr_name += ['Majority_Vote']
+all_clsfrs = [lr_pipe, knn_pipe, dt_clf, ens]
 
 # get roc_auc score over 10 folds for base and ensemble classifiers
 print('**********************\ncompare ensemble performance\n**********************\n')
-for _clf_nm, _clf in zip(clsfr_name, [lr_pipe, knn_pipe, dt_clf, ens]):
+for _clf_nm, _clf in zip(clsfr_name, all_clsfrs):
     score = cross_val_score(
         estimator=_clf,
         X = X,
@@ -81,4 +89,3 @@ for _clf_nm, _clf in zip(clsfr_name, [lr_pipe, knn_pipe, dt_clf, ens]):
         scoring = 'roc_auc'
     )
     print('roc_auc over 10 folds for base {}: {} +/- {}'.format(_clf_nm, np.mean(score), np.std(score)))
-
